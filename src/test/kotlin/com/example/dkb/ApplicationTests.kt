df4a685848aadbec8c8c16583779420305c751ed
muhammad <bezedkhan@gmail.com>
2025-04-18 15:43:28 +0200

    1st commit - base project - starter

---
+++ src/test/kotlin/com/example/dkb/ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.dkb
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
